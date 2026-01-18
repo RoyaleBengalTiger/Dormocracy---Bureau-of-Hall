2c8461b7f21f303edc10af1d3ac2b0778a5a27ed
RoyaleBengalTiger <siamsifat244@gmail.com>
2026-01-19 00:21:32 +0600

    first commit - setup done

---
+++ backend/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>('PORT') ?? 3000;
+
+  await app.listen(port, '0.0.0.0');
+}
+bootstrap();
